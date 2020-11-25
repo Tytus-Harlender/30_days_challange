5b0d3d4b00214e2983eac8c78a3b37a4920ab0f6
Tytus Harlender <64841178+Tytus-Harlender@users.noreply.github.com>
2020-11-25 20:10:38 +0100

    Update Day1.cs

--- Day1/Day1.cs
+++ Day1/Day1.cs
@@ -1,35 +1,107 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System;
+
 
 namespace _30_days_challange
-{
-    class Day1
+{   
+    
+    public class Day1
     {
-        class Car
+
+        public class PolonezTemplate
         {
 
-            public void model()
-            { }
+            public string brand = "Polonez";
+            public string color = "red";
+            public double doors = 5;
+            public double generation = 4;
 
+        }
+
+
+        public abstract class CarPrototype
+        {
+         
+            public abstract void SwitchTheLights();
+            public void Go()
+            {
+                Console.WriteLine("Car is going!!!");
+            }
 
 
         }
 
-        abstract class CarModel
+        public class PolonezType : CarPrototype
         {
+            public override void SwitchTheLights()
+            {
+                Console.WriteLine("Polonez switches the lights automatically after engine start");
+            }
 
+        }
+
+        public class VolkswagenType : CarPrototype
+        {
+            public override void SwitchTheLights()
+            {
+                Console.WriteLine("Volkswagen switches the lights after the key input to the ignition switch");
+            }
+
+        }
+
+        public interface IConceptCar
+        {
+            void CarFunction();
+            void CarFunctionTwo();
 
 
         }
 
-        interface IConceptCar
+        public class CarImplementation : IConceptCar
         {
+            
+            public void CarFunction()
+            {
+                Console.WriteLine("Movement from point to point");
+            }
 
-
-
-
+            public void CarFunctionTwo()
+            {
+                Console.WriteLine("Playing music");
+            }
         }
 
+        //multi-implementation presented
+
+        public interface IConceptPlane 
+        {
+            public void PLaneFunctionOne();
+            public void PlaneFunctionTwo();
+        
+        
+        }
+
+        public class PlanCarCross : IConceptCar, IConceptPlane
+        {
+            public void CarFunction()
+            {
+                Console.WriteLine("Riding on the ground");
+            }
+
+            public void CarFunctionTwo()
+            {
+                Console.WriteLine("Having tires");
+            }
+
+            public void PLaneFunctionOne()
+            {
+                Console.WriteLine("Flying to the sky");
+            }
+
+            public void PlaneFunctionTwo()
+            {
+                Console.WriteLine("Having wings");
+            }
+        }
     }
 }
+
