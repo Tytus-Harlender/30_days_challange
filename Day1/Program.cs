ded207a3d0cc7abc0de1944012bd29ea5bae5132
Tytus Harlender <64841178+Tytus-Harlender@users.noreply.github.com>
2020-11-25 20:11:31 +0100

    Update Program.cs

--- Day1/Program.cs
+++ Day1/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace _30_days_challange
 {
@@ -7,8 +7,47 @@ namespace _30_days_challange
     {
         static void Main()
         {
+            DayOneProgram();
+        }
 
-            Console.WriteLine("Hello World!");
+        static void DayOneProgram()
+        {
+            //createing an instance of a regular class
+
+            Day1.PolonezTemplate polonezTemplate = new Day1.PolonezTemplate();
+
+            Console.WriteLine(polonezTemplate.brand);
+            Console.WriteLine(polonezTemplate.color);
+            Console.WriteLine(polonezTemplate.doors);
+            Console.WriteLine(polonezTemplate.generation);
+
+            //createing an instance of a subclass (a derivative of an abstract class) 
+
+
+            Day1.PolonezType polonezType = new Day1.PolonezType();
+            Day1.VolkswagenType volkswagenType = new Day1.VolkswagenType();
+
+            polonezType.SwitchTheLights();
+            polonezType.Go();
+
+            volkswagenType.SwitchTheLights();
+            volkswagenType.Go();
+
+            //createing an instance of a subclass(a implementation of an interface) 
+
+            Day1.CarImplementation carImplementation = new Day1.CarImplementation();
+
+            carImplementation.CarFunction();
+            carImplementation.CarFunctionTwo();
+
+            Day1.PlanCarCross planCarCross = new Day1.PlanCarCross();
+
+            planCarCross.CarFunction();
+            planCarCross.CarFunctionTwo();
+            planCarCross.PLaneFunctionOne();
+            planCarCross.PlaneFunctionTwo();
+
+
         }
     }
 }
