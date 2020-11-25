d6f222e829a2bbb0f9154b437c555fccc9aa18bb
Tytus Harlender <tytus.harlender95@gmail.com>
2020-11-25 12:10:16 +0100

    Add project files.

---
+++ 30_days_challange/Day1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _30_days_challange
+{
+    class Day1
+    {
+        class Car
+        {
+
+            public void model()
+            { }
+
+
+
+        }
+
+        abstract class CarModel
+        {
+
+
+
+        }
+
+        interface IConceptCar
+        {
+
+
+
+
+        }
+
+    }
+}
---
+++ 30_days_challange/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _30_days_challange
+{
+
+    class Program
+    {
+        static void Main()
+        {
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
