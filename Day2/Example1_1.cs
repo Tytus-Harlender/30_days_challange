b07dab6740e81350d88bf03bf28c064369d4324b
Tytus Harlender <64841178+Tytus-Harlender@users.noreply.github.com>
2020-11-26 22:05:53 +0100

    Create Example1_1.cs

---
+++ Day2/Example1_1.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day2
+{
+    
+    class Example1_1
+    {
+
+        public static void ShowExampleOne()
+        {
+            
+            DisplayListOfCars(CreateListOfCars());
+
+        }
+
+        public static List<string> CreateListOfCars()
+        {
+            List<string> carsList = new List<string>();
+
+            carsList.Add("Polonez");
+            carsList.Add("VolksWagen");
+            carsList.Add("Ford");
+            carsList.Add("Audi");
+
+            return carsList;
+
+        }
+
+        public static void DisplayListOfCars(List<string> cars) 
+        {
+            foreach (string name in cars)
+            {
+                Console.WriteLine(name);
+            }
+        
+        }
+
+        public static List<string> CreateListOfCarsSimpler()
+        {
+            List<string> carsList = new List<string>() {"Polonez", "VolksWagen", "Ford", "Audi"};
+
+            return carsList;
+        }
+
+    }
+}
