4a18479f40a15ba96d117e73a7a49fbce3a30c0b
Tytus Harlender <64841178+Tytus-Harlender@users.noreply.github.com>
2020-11-27 23:40:14 +0100

    Create Example4.cs
    Example 4 showing the creation of Dictionary<IKey,IValue> class

---
+++ Day2/Example4.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day2
+{
+    class Example4
+    {
+
+
+        public Example4()
+        {
+            CreateDictionary();
+        
+        }
+        public void CreateDictionary()
+        {
+            Dictionary<int, string> theRaceResults = new Dictionary<int, string>();
+
+            theRaceResults.Add(1, "Adam");
+            theRaceResults.Add(2, "Tom");
+            theRaceResults.Add(3, "Rafael");
+            theRaceResults.Add(4, "Michael");
+            theRaceResults.Add(5, "Gabriel");
+            
+            
+        }
+        
+    }
+}
