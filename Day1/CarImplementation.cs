9c1b2fe39aaabc17b70d81223ae86cde77c4db13
Tytus Harlender <tytus.harlender95@gmail.com>
2020-12-01 18:46:27 +0100

    Folder Day1 restored

---
+++ Day1/CarImplementation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Day1
+{
+    interface CarImplementation:IConceptCar
+    {
+
+        public void CarFunction()
+        {
+            Console.WriteLine("Movement from point to point");
+        }
+
+        public void CarFunctionTwo()
+        {
+            Console.WriteLine("Playing music");
+        }
+    }
+
+}
---
+++ Day1/VolkswagenType.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Day1
+{
+    class VolkswagenType:CarPrototype
+    {
+        public override void SwitchTheLights()
+        {
+            Console.WriteLine("Volkswagen switches the lights after the key input to the ignition switch");
+        }
+    }
+}
