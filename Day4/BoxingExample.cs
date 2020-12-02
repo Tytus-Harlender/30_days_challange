1b274d9ce5a91515bda0b93e14bab275d2dea5e9
Tytus Harlender <tytus.harlender95@gmail.com>
2020-12-02 23:59:36 +0100

    new two classes were added

---
+++ Day4/BoxingExample.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day4
+{
+    class BoxingExample
+    {
+        public void BoxingA()
+        {
+            int a = 5;
+            
+            object o = a;
+
+        }
+        
+
+    }
+}
