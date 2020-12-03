13d3b10d8d73c9e033849233bc949dfc5b7b1076
Tytus Harlender <tytus.harlender95@gmail.com>
2020-12-03 23:37:55 +0100

    Boxing example modification

--- Day4/BoxingExample.cs
+++ Day4/BoxingExample.cs
@@ -1,18 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Day4
 {
     class BoxingExample
     {
         public void BoxingA()
-        {
+        {   
             int a = 5;
             
             object o = a;
 
+            a = 10;
+
+            Console.WriteLine("a");
+            Console.WriteLine("o");
+            
         }
+
+      
         
 
     }
