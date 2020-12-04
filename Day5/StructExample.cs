20d2ffa09ac8a82f6eb1745d8e5bce4cf09512e6
Tytus Harlender <tytus.harlender95@gmail.com>
2020-12-04 19:59:43 +0100

    New folder for Day5 created. Struct described with an example

---
+++ Day5/StructExample.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Day5
+{
+    class StructExample
+    {
+
+        struct SimpleExample
+        {
+
+            public int number;
+            public string word;
+            public bool TheAnswer;
+        
+        }
+
+        public void UseAStruct()
+        {
+            SimpleExample s;
+            s.number = 5;
+            s.word = "That's it!";
+            s.TheAnswer = false;
+
+            Console.WriteLine(s.number);
+            Console.WriteLine(s.TheAnswer);
+
+        }
+
+
+
+
+
+
+
+    }
+}
