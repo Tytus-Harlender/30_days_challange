ff4454ac847fa455f7728f3452090ca51565b30d
Tytus Harlender <tytus.harlender95@gmail.com>
2020-12-06 18:52:01 +0100

    Sealed keyword usage explained with StopInheritance class

---
+++ Day5/StopInheritance.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Day5
+{
+    abstract class FirstClass
+    {
+        public abstract void F();
+        public abstract void F2(); 
+    }
+
+    class StopInheritance : FirstClass
+    {
+        public sealed override void F() { Console.WriteLine("Y.F"); }
+        public override void F2() { Console.WriteLine("Y.F2"); }
+    }
+
+    class FinalCLass : StopInheritance
+    {
+        //'public override void F() { Console.WriteLine("Z.F1"); }' would generate error
+        public override void F2() { Console.WriteLine("Z.F2"); }
+    }
+}
