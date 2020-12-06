43d9de923fe9786273ce37cfed74d765b701f3f4
Tytus Harlender <tytus.harlender95@gmail.com>
2020-12-06 20:13:08 +0100

    overriding and overloading of methods explained

---
+++ Day5/OverloadingExample.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Day5
+{
+    class OverloadingExample
+    {
+        public void PrintSomething(int something)
+        {
+            Console.WriteLine(something);
+        }
+        public void PrintSomething(double something)
+        {
+            Console.WriteLine(something);
+        }
+
+    }
+}
---
+++ Day5/OverridingExample.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Day5
+{
+    abstract class OverridingExample
+    {
+        public abstract void CallMe();
+    }
+
+    class OverridingClass:OverridingExample
+    {
+
+        public override void CallMe()
+        {
+            Console.WriteLine("I call you");
+        }
+
+    }
+
+}
