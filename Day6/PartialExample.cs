d6d2965b77bd4dcb213364734cccb37d2b88df38
Tytus Harlender <tytus.harlender95@gmail.com>
2020-12-07 23:34:00 +0100

    PartialExample class added

---
+++ Day6/PartialExample.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Day6
+{
+    partial class PartialExample
+    {
+        public PartialExample(int x, int y)
+        {
+            X = x;
+            Y = y;
+        
+        }
+
+        public void ShowTheValues()
+        {
+            Console.WriteLine(X);
+            Console.WriteLine(Y);
+        
+        }
+    }
+
+    partial class PartialExample
+    {
+        
+        private int x;
+        private int y;
+        public int X { get => x; set => x = value; }
+        public int Y { get => y; set => y = value; }
+    }
+}
