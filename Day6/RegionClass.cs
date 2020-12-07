d8b5ea3b40b562df384d3a9b1f2d2ed26685a2b1
Tytus Harlender <tytus.harlender95@gmail.com>
2020-12-07 22:52:49 +0100

    RegionExample class added

---
+++ Day6/RegionClass.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Day6
+{
+    class RegionClass
+    {
+
+        #region Fields
+
+        private string name;
+        private int age;
+        private double weight;
+        private string colour;
+
+
+        #endregion
+
+        #region Propierties
+
+        public string Name { get => name; set=> name = value; }
+        public int Age { get =>age; set=> age = value; }
+        public double Weight { get => weight; set => weight = value; }
+        public string Colour { get => colour; set => colour = value; }
+        #endregion
+
+        #region Methods
+
+        public void DisplayNote()
+        {
+            Console.WriteLine("This is a note");
+        
+        }
+
+        public int GiveMeAgeTimesTwo()
+        {
+            return 2*age;
+        }
+
+        #endregion
+
+    }
+}
