d486a419239d2818da826fc6f9b6b918a7ecf5ed
Tytus Harlender <tytus.harlender95@gmail.com>
2020-12-08 23:28:52 +0100

    class StringBuilderExample added

---
+++ Day7/StringBuilderClassExample.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day7
+{
+    class StringBuilderClassExample
+    {
+        public static void StringBuilderExample()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("Line 1:");
+            sb.AppendLine("Line 2:");
+
+            string allLines = sb.ToString();
+        }
+
+        public static void StringExample()
+        {
+            string s = string.Empty;
+            s += "Line 1:";
+            s += "/r/nLine 2:/r/n"; 
+
+        }
+        
+    }
+}
