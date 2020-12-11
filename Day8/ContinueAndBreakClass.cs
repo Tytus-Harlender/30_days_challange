15c2f158631d66d19e5a9b8810f617d1a39aa2ce
Tytus Harlender <tytus.harlender95@gmail.com>
2020-12-11 21:24:51 +0100

    classes for point fourth - nullable types and point fifth - break and continue difference added

---
+++ Day8/ContinueAndBreakClass.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+
+namespace Day8
+{
+    class ContinueAndBreakClass
+    {
+        public static void BreakingLoop()
+        {
+            int i = 0;
+            while (i < 10)
+            {
+                Console.WriteLine(i);
+                i++;
+                if (i == 4)
+                {
+                    break;
+                }
+            }
+        }
+
+        public static void OmittingIteration()
+        {
+
+            int i = 0;
+            while (i < 10)
+            {
+                if (i == 4)
+                {
+                    i++;
+                    continue;
+                }
+                Console.WriteLine(i);
+                i++;
+            }
+
+
+
+        }
+
+
+    }
+}
---
+++ Day8/NullableValueClass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day8
+{
+    class NullableValueClass
+    {
+        public void ShowNullableExample()
+        {
+
+            int? nullableInt;
+            Nullable<int> nullableIntTwo = null;
+            int? a = 5;
+
+            if (a.HasValue)
+            {
+                Console.WriteLine($"a is {a.Value}");
+            }
+            else
+            {
+                Console.WriteLine("a does not have a value");
+            }
+
+
+        }
+
+    }
+}
