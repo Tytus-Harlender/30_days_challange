dde3dde5ecbc681e952e8d8084f8e34dcddb352d
Tytus Harlender <tytus.harlender95@gmail.com>
2020-12-12 23:40:33 +0100

    class NamespaceDelarationClass added

---
+++ Day9/NamespaceDeclarationExample.cs
@@ -0,0 +1,21 @@
+﻿
+
+namespace Day9
+{
+
+    class SampleClass { }
+
+    interface ISampleInterface { }
+
+    struct SampleStruct { }
+
+    enum SampleEnum { a, b }
+
+    delegate void SampleDelegate(int i);
+
+    namespace Nested
+    {
+        class SampleClass2 { }
+    }
+
+}
