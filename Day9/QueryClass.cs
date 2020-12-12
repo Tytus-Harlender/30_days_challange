7bdff53c4f6da204fdd48423db3617c8d8509894
Tytus Harlender <tytus.harlender95@gmail.com>
2020-12-12 22:24:04 +0100

    QueryClass added

---
+++ Day9/QueryClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace Day9
+{
+    class QueryClass
+    {
+        static void ShowQueryExample()
+        {
+            
+            int[] numbers = new int[7] { 0, 1, 2, 3, 4, 5, 6 };
+
+            var numQuery =
+                from num in numbers
+                where (num % 2) == 0
+                select num;
+
+            foreach (int num in numQuery)
+            {
+                Console.Write("{0,1} ", num);
+            }
+
+        }
+    }
+}
