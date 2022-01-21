a6e75d65ce9dc72d8fc9a1e679774c71f75fde1c
Tytus Harlender <tytus.harlender95@gmail.com>
2022-01-21 11:12:19 +0100

    generic delegates presented

---
+++ Day12/GenericDelegatePresentation.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Day12
+{
+    public delegate T add<T>(T param1, T param2);
+    public class GenericDelegatePresentation
+    {
+        public void PresentGenericDelegate()
+        {
+            add<int> sum = Sum;
+            Console.WriteLine(sum(10,20));
+            add<string> con = Concat;
+            Console.WriteLine(con("Hello","World!"));
+        }
+
+        public static int Sum(int val1, int val2)
+        {
+            return val1 + val2;
+        }
+
+        public static string Concat(string str1, string str2)
+        {
+            return str1 + str2;
+        }
+    }
+}
