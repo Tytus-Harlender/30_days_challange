2d2fc0bfc4029608d5d5dc26a66f9f56c9379206
Tytus Harlender <tytus.harlender95@gmail.com>
2022-02-02 17:27:56 +0100

    ThreadPoolPresentation class created with two methods

---
+++ Day14/ThreadPoolPresentation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+
+namespace Day14
+{
+    public class ThreadPoolPresentation
+    {
+        public static void BackgroundTask(Object stateInfo)
+        {
+            Console.WriteLine("Hello! I'm a worker from ThreadPool");
+            Thread.Sleep(1000);
+        }
+
+        public static void PresentThreadPool()
+        {
+            ThreadPool.QueueUserWorkItem(BackgroundTask);
+            Console.WriteLine("Main thread does some work, then sleeps.");
+            Thread.Sleep(500);
+            Console.WriteLine("Main thread exits.");
+            Console.ReadKey();
+
+        }
+    }
+}
